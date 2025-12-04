b5b3c5fe237aee29c5a4b88366028276587e7927
EmilMov <arsenemil86@gmail.com>
2025-12-04 22:15:42 +0400

    Implement file activity watcher using inotify

---
+++ hw11-Mishiks-Adventures/hw11.cpp
@@ -0,0 +1,60 @@
+#include <iostream>
+#include <unistd.h>
+#include <sys/inotify.h>
+
+int main(int size ,char* arr[]){
+    if(size != 2){
+        return 1;
+    }
+    const int fd = inotify_init();
+    if(fd < 0){
+        std::cout<<"fd is < 0";
+        return 1;
+    }
+    if(const int wd = inotify_add_watch(fd,arr[1],IN_ALL_EVENTS);wd < 0){
+        std::cout<<"wd is < 0";
+        return 1;
+    }
+    
+    std::cout<<"Watching "<< arr[1]<<std::endl;
+    
+    char buffer[4096];
+    
+    while(true){
+        const int lenght = read(fd,buffer,sizeof(buffer));
+        if(lenght < 0){
+            break;
+        }
+        
+        int i = 0;
+        while(i < lenght){
+            const inotify_event* event = (struct inotify_event*)&buffer[i];
+            
+            std::cout<<"Noticed activity in ";
+            
+            if(event->len > 0){
+                std::cout<<event->name<<std::endl;
+            }
+
+            if(event->mask & IN_CREATE){
+                std::cout<<"File created"<<std::endl;
+            }
+            if(event->mask & IN_MODIFY){
+                std::cout<<"File modified"<<std::endl;
+            }
+            if(event->mask & IN_DELETE){
+                std::cout<<"File deleted"<<std::endl;
+            }
+            if(event->mask & IN_OPEN){
+                std::cout<<"Why are you gay???"<<std::endl;
+            }
+            if (event->mask & IN_ACCESS) {
+                std::cout << "File accessed" << std::endl;
+            }
+            
+            i+= sizeof(inotify_event) + event->len;
+        }
+    }
+    close(fd);
+    return 0;
+}
